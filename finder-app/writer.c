4ad68f0fcb31917863d5eee2c1e1dcd5f5aeb81a
Jacques van der Merwe <jacques.cgi@gmail.com>
2024-01-28 06:50:32 +0000

    Add syslog support

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,6 +1,5 @@
 #include <sys/fcntl.h>
-
-#include <stdio.h>
+#include <sys/syslog.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,6 +9,7 @@ int
 main(int argc, char *argv[]) {
     int result = 0;
 
+    openlog(NULL, LOG_PERROR, LOG_USER);
     if (argc < 3) {
         printf("USAGE: writer [write_file] [value_to_write]\n");
         result = 1;
@@ -17,14 +17,15 @@ main(int argc, char *argv[]) {
         int fd = open(argv[1], O_WRONLY | O_CREAT, 0755);
         if (fd == -1) {
             result = 1;
-            printf("%s\n", strerror(errno));
+            syslog(LOG_ERR, "%s\n", strerror(errno));
         } else {
             size_t len = strlen(argv[2]);
             write(fd, argv[2], len);
+            syslog(LOG_DEBUG, "Writing %s to %s", argv[1], argv[2]);
             close(fd);
         }
     }
+    closelog();
 
-
-	return(result);
+    return(result);
 }
